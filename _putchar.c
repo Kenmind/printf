1d9c41b713f73b20cb25d66104fe60bb0d84d79a
Kennedy Kalaluka <kennedykalaluka04@gmail.com>
2021-10-04 13:23:32 +0100

    Added _printf.c, func.c, get_func.c, and corrected betty style in _putchar.c and finished main.h

--- _putchar.c
+++ _putchar.c
@@ -5,7 +5,7 @@
  * @c: Character to print
  *
  * Return: On success 1
- * 		On Error, -1 is returned, and errno is set appropriately
+ *		On Error, -1 is returned, and errno is set appropriately
  */
 
 int _putchar(char c)
--- main.h
+++ main.h
@@ -31,6 +31,15 @@ int _printf(const char *format, ...);
 int print_c(va_list parameters);
 int print_s(va_list parameters);
 int print_percent(__attribute__((unused))va_list parameters);
-
+int print_i(va_list parameters);
+int print_d(va_list parameters);
+int (*get_func(const char a))(va_list);
+int print_integer(va_list parameters);
+int print_decimal(va_list parameters);
+int print_binary(va_list parameters);
+int binary_recursive(unsigned int num, int len);
+int print_unint(va_list parameters);
+int print_octal(va_list args);
+int rot_13(va_list parameters);
 
 #endif /* MAIN_H */
