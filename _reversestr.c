5ef87887ea85257c5cf2990035bc64d6b8a2816e
Oluchi Onyekpa <onyekpaoluchi@gmail.com>
2021-10-05 09:16:02 +0100

    Added the reversed string file

--- _reversestr.c
+++ _reversestr.c
@@ -24,3 +24,19 @@ int print_r(va_list list)
 	}
 	return (i);
 }
+
+void rev_string(char *s)
+{
+	int length = _strlen(s);
+	int counter = 0;
+	char tmp;
+
+	while (counter < length / 2)
+	{
+		tmp = *(s + counter);
+		*(s + counter) = *(s + length - 1 - counter);
+		*(s + length - 1 - counter) = tmp;
+		counter++;
+	}
+
+}
